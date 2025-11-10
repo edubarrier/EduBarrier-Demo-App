cda58a8b12a16a159affa025876b68fe75a85f2c
edubarrier <lcoyne@edubarrier.com>
2025-11-10 13:16:41 -0500

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    // Generate unique filenames for each build to bust cache
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].[hash].js`,
+        chunkFileNames: `assets/[name].[hash].js`,
+        assetFileNames: `assets/[name].[hash].[ext]`
+      }
+    }
+  },
+  server: {
+    // Disable caching in development
+    headers: {
+      'Cache-Control': 'no-store'
+    }
+  }
+})
